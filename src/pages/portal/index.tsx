569eb931fb870cdf72838a0a49f3c46d07b35026
ayrtongp <ayrtongp@gmail.com>
2023-05-23 13:27:22 -0300

    23052023

--- src/pages/portal/index.tsx
+++ src/pages/portal/index.tsx
@@ -12,7 +12,7 @@ import DashboardsSinaisVitais from '@/components/Dashboard/DashboardsSinaisVitai
 // import DashboardCard01 from '../partials/dashboard/DashboardCard01';
 // import Banner from '../partials/Banner';
 
-function index() {
+function Index() {
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -29,7 +29,7 @@ function index() {
             <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
               {/* Welcome banner */}
               <WelcomeBanner />
-              {/* index actions */}
+              {/* Index actions */}
               <div className="sm:flex sm:justify-between sm:items-center mb-8">
                 {/* Left: Avatars */}
                 {/* <DashboardAvatars /> */}
@@ -57,4 +57,4 @@ function index() {
   );
 }
 
-export default index;
+export default Index;
