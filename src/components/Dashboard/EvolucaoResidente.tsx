87699506ed642145aa00a13b27b085552883616f
ayrtongp <ayrtongp@gmail.com>
2023-06-05 18:53:34 -0300

    05062023

--- src/components/Dashboard/EvolucaoResidente.tsx
+++ src/components/Dashboard/EvolucaoResidente.tsx
@@ -1,9 +1,6 @@
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-import { formatDateBR, formatDateBRHora, pillsBadge } from '@/utils/Functions';
-import { FaEye } from 'react-icons/fa';
-import { BsArrowUpRightCircleFill } from 'react-icons/bs';
-import { Tooltip } from 'react-tooltip';
+import { formatDateBR, formatDateBRHora } from '@/utils/Functions';
 import 'react-tooltip/dist/react-tooltip.css'
 import axios from 'axios';
 import { notifyError } from '@/utils/Functions';
@@ -42,31 +39,25 @@ const EvolucaoResidente = () => {
     <div className="col-span-full bg-white shadow-lg rounded-sm border border-slate-200">
       <header className="px-5 py-4 border-b border-slate-100">
         <h2 className="font-semibold text-slate-800 text-center">Hoje, {dateHoje}</h2>
-        <h2 className="font-semibold mt-1 text-xs text-red-500">FALTA CADASTRO: ANOTAÇÕES DE ENFERMAGEM</h2>
+        <h2 className="font-semibold mt-1 text-xs text-red-500">Se a data estiver em vermelho, significa que falta cadastrar para o idoso</h2>
       </header>
       <div className="p-3">
 
         {/* Table */}
         <div className="overflow-x-auto">
-          <table className="w-full whitespace-nowrap border">
+          <table className="table-auto w-full whitespace-nowrap border border-collapse">
 
             {/* Table header */}
             <thead className="text-xs uppercase text-slate-400 bg-slate-200 rounded-sm">
               <tr className='text-center'>
                 <th className="p-2">
-                  <div className="font-semibold text-center">Idoso</div>
+                  <div className="font-semibold text-center">Nome do Residente</div>
                 </th>
                 <th className="p-2">
-                  <div className="font-semibold text-center">anot nome</div>
+                  <div className="font-semibold text-center">Anotações de Enfermagem</div>
                 </th>
                 <th className="p-2">
-                  <div className="font-semibold text-center">anot data</div>
-                </th>
-                <th className="p-2">
-                  <div className="font-semibold text-center">sinais nome</div>
-                </th>
-                <th className="p-2">
-                  <div className="font-semibold text-center">sinais data</div>
+                  <div className="font-semibold text-center">Sinais Vitais</div>
                 </th>
               </tr>
             </thead>
@@ -87,30 +78,30 @@ const EvolucaoResidente = () => {
                   const hoursDifference = timeDifference / (1000 * 60 * 60);
                   const testDateSinais = hoursDifference > 6
 
+                  const dataAnotacao = item.lastEntryAnotacoes?.data
+
                   return (
                     // /* Row */
                     <tr key={index} className={`text-center ${index % 2 == 0 ? 'bg-slate-100' : ''}`}>
                       <td className="p-2 overflow-hidden max-w-[150px]">
-                        <div className="flex items-center">
+                        <Link href={`/portal/residentes/${item._id}`} className="flex items-center">
                           <Image width={40} height={40} className='rounded-full w-10 h-10 shrink-0 mr-2' src={foto} alt={`${item.nome} Idosa Lar Felizidade`} />
                           <div className="text-slate-800">{item.nome}</div>
-                        </div>
+                        </Link>
                       </td>
-                      <td className={`pl-1`}>
-                        <span className={`${testDateAnot ? 'bg-red-500 rounded px-2 py-1' : ''}`}>
-                          {formatDateBR(item.lastEntryAnotacoes?.data)}
+                      <td className={`pl-3`}>
+                        <span className={`mr-1 ${testDateAnot ? 'bg-red-500 rounded px-2 py-1' : ''}`}>
+                          {dataAnotacao ? formatDateBR(dataAnotacao) : ""}
                         </span>
+                        <span className='mr-1'>-</span>
+                        <span>{item.lastEntryAnotacoes?.usuario_nome}</span>
                       </td>
-                      <td className='pl-1 max-w-[50px] overflow-hidden'>
-                        {item.lastEntryAnotacoes?.usuario_nome}
-                      </td>
-                      <td className='pl-1'>
-                        <span className={`${testDateSinais ? 'bg-red-500 rounded px-2 py-1' : ''}`}>
+                      <td className='pl-3'>
+                        <span className={`mr-1 ${testDateSinais ? 'bg-red-500 rounded px-2 py-1' : ''}`}>
                           {item.lastEntrySinais?.createdAt}
                         </span>
-                      </td>
-                      <td className='pl-1 max-w-[50px] overflow-hidden'>
-                        {item.lastEntrySinais?.usuario_nome}
+                        <span className='mr-1'>-</span>
+                        <span>{item.lastEntrySinais?.usuario_nome}</span>
                       </td>
                     </tr>
                   )
