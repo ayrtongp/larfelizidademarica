9e46c663da9ee8834970f3921bdc7944c2669d24
ayrtongp <ayrtongp@gmail.com>
2023-06-21 00:42:27 -0300

    21062023

--- src/components/Residentes/ModalAlergias.tsx
+++ src/components/Residentes/ModalAlergias.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import Button from '@material-ui/core/Button';
 import { FaTrash } from "react-icons/fa";
 
 export default function ModalResidente({ children }: any) {
@@ -17,19 +15,6 @@ export default function ModalResidente({ children }: any) {
         {children}
       </button>
 
-      <Dialog open={showModal} onClose={() => setShowModal(false)}>
-        <div className="bg-white p-4 rounded min-w-[240px] ">
-          <h2 className="text-lg mb-2 font-bold text-red-500">{vars.titulo}</h2>
-          {vars.lista.map((item, index) => {
-            return (
-              <p key={index} className='mb-1'><span>{index + 1} - </span>{item}</p>
-            )
-          })}
-          <div className="mt-2 flex justify-center">
-            <Button variant="outlined" color="primary" onClick={() => setShowModal(false)}>Fechar</Button>
-          </div>
-        </div>
-      </Dialog>
     </div>
   );
 }
