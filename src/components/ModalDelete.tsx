5197b5f9e631ab0ed09ba38921c10103e550be2c
ayrtongp <ayrtongp@gmail.com>
2023-06-21 00:41:00 -0300

    21062023

--- src/components/ModalDelete.tsx
+++ src/components/ModalDelete.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import Button from '@material-ui/core/Button';
 import { FaTrash } from "react-icons/fa";
 
 export default function DeleteButton({ onConfirm, idosoData, id, url }: any) {
@@ -23,17 +21,6 @@ export default function DeleteButton({ onConfirm, idosoData, id, url }: any) {
         <FaTrash />
       </button>
 
-      <Dialog open={showModal} onClose={() => setShowModal(false)}>
-        <div className="bg-white p-4 rounded">
-          <h2 className="text-lg mb-2 font-bold text-red-500">Confirmação de Exclusão</h2>
-          <p className="mb-4">Você tem certeza que deseja deletar o item abaixo?</p>
-          <p>{itemToDelete}</p>
-          <div className="mt-2 flex justify-between">
-            <Button variant="outlined" color="primary" onClick={() => setShowModal(false)}>Cancel</Button>
-            <Button variant="contained" color="primary" onClick={handleDelete}>Delete</Button>
-          </div>
-        </div>
-      </Dialog>
     </div>
   );
 }
